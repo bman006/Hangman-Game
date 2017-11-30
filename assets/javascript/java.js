6ab76ce1e4ea54498b2f914ce5a58f2e7eb76266
Brian Parkes <parkesbrianj@gmail.com>
2017-11-29 18:31:20 -0600

    Coded how to pull random word from bank but discovered infinite loop when all words from array have already been used

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,4 +1,4 @@
-//Declare all variables
+//Declare all global variables
 	//Store all possible words for use in the hangman game
 	var wordBank = ["one", "two", "three", "four", "five"];
 	//Check which words have already been used by the game
@@ -6,7 +6,7 @@
 	var wordUsed = [];
 		//Genereate full wordUsed array based on current wordBank array length
 		for (var i = 0; i < wordBank.length; i++) {
-			wordUsed.push(0);
+			wordUsed.push("unused");
 		}
 	
 	//Store current word being used by the game
@@ -19,6 +19,8 @@
 	var wins = 0;
 	var losses = 0;
 
+	//loop variable for checking if a word has already been used
+	var wordIndex = -1;
 
 //Initiate game
 	function startButton() {
@@ -29,8 +31,19 @@
 		else {
 			firstGame = false;
 		}
-	}
 
-// losses++;
-// console.log(losses);
+		//Select word at random from wordBank
+		var newWord = false;
+		while (newWord === false) {
+			wordIndex = Math.floor(Math.random() * wordBank.length)
+			if(wordUsed[wordIndex] === "unused") {
+				wordActive = wordBank[wordIndex];
+				wordUsed[wordIndex] = "used";
+				newWord = true;
+			}
+			
+		}
+	console.log(losses);
+	console.log(wordActive);
+	}
 
