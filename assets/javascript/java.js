4d36dfb8b0a781cc860dea0db83a696ef784b291
Brian Parkes <parkesbrianj@gmail.com>
2017-12-02 09:51:38 -0600

    Still debugging wordDisplay array that is resetting to empty with onkeyup event

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,14 +1,18 @@
 //Declare all global variables
 	//Store all possible words for use in the hangman game
 	var wordBank = ["one", "two", "three", "four", "five"];
-	//Check which words have already been used by the game
 
+	//Check which words have already been used by the game
 	var wordUsed = [];
 		//Genereate full wordUsed array based on current wordBank array length
 		for (var i = 0; i < wordBank.length; i++) {
 			wordUsed.push("unused");
 		}
-	
+	var wordDisplay = []; //Array for displaying word on screen, is modified while user guesses
+
+	//Array to store what letters have already been guessed
+	var usedLetters = [];
+
 	//Store current word being used by the game
 	var wordActive = "original";
 
@@ -24,7 +28,14 @@
 
 //Initiate game
 	function startButton() {
-		//If not the first game, increment the losses counter
+	//Declare variables
+		var wordDisplay = []; //Clear the array
+		var wordHTML = "";
+		var newWord = false;
+		var usedLetters = []; //Clear the array
+
+
+	//If not the first game, increment the losses counter
 		if (firstGame === false) {
 			losses++;
 		}
@@ -33,7 +44,6 @@
 		}
 
 	//Select word at random from wordBank
-		var newWord = false;
 		//If all words have already been used then skip while loop to avoid infinite loop
 		if (wordUsed.includes("unused")) {
 			while (newWord === false) {
@@ -47,18 +57,30 @@
 		}
 
 	//Create array with enough "_" characters for entire word
-		//Array for displaying word on screen, is modified while user guesses
-		var wordDisplay = [];
-		var wordHTML = "";
 		//Generate "_" with correct length
 		for (i=0; i < wordActive.length; i++) {
 			wordDisplay.push(" _");
 		}
-		wordHTML = "<h3>" + wordDisplay.join("") + "</h3>";
-		document.getElementsByClassName("current-word")[0].innerHTML = wordHTML;
-
-	console.log(losses);
-	console.log(wordActive);
-	console.log(wordDisplay);
+		document.getElementsByClassName("current-word")[0].innerHTML = "<h3>" + wordDisplay.join("") + "</h3>";
+console.log(wordActive);
+console.log(wordDisplay);	
 	}
 
+//Trigger for when user inputs a letter
+	document.onkeyup = function(event) {
+		console.log(wordDisplay);
+		var letterGuessed = event.key;
+		console.log(wordDisplay);
+		//Check if letter has already been guessed
+		if (usedLetters.indexOf(letterGuessed) === -1) {
+			usedLetters.push(letterGuessed); //Add letter to list of used letters
+			for (i=0; i < wordActive.length; i++) {
+				if (wordActive.charAt(i) === letterGuessed) {
+					wordDisplay[i] = letterGuessed;
+				}
+			}
+		document.getElementsByClassName("current-word")[0].innerHTML = "<h3>" + wordDisplay.join("") + "</h3>";
+		}
+
+
+	}
